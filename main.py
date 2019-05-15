37f6030a41556f5bda405fceacfa55d41e7d2324
Ntsiki-Nonjo <815108@students.wits.ac.za>
2019-05-15 13:17:59 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
+import pandas as pd
+import numpy as np
 
-from models.DecisionTreeAlgorithm import *
-
-a = DecisionTreeAlgorithm('a')
-a.print_csv_head()
+data = pd.read_csv('data/adult.csv')
+data = data[data['age'] == 17]
+other, count = np.unique(data['class'], return_counts=True)
